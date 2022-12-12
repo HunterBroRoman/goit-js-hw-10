67a7e66ee272b93151b5b466064cee17c84c358d
Roman <sayga76239@gmail.com>
2022-12-12 12:39:54 +0100

    fdfdfd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+
 
 const DEBOUNCE_DELAY = 300;
+
+// .then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+
+
+
+
+// "Too many matches found. Please enter a more specific name."
+// "Oops, there is no country with that name"
